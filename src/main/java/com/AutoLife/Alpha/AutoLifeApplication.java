e3a926872bfaa9ee480d47bac91358fc5ad3fc64
Luis Andres Gonzalez Corzo <luisangoco26@gmail.com>
2024-03-20 14:56:38 -0500

    Init the proyect

---
+++ src/main/java/com/AutoLife/Alpha/AutoLifeApplication.java
@@ -0,0 +1,13 @@
+package com.AutoLife.Alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoLifeApplication.class, args);
+	}
+
+}
